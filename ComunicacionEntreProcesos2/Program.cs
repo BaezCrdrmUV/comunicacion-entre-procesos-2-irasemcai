fabd08f2f96fec7704b7f0e4f5187a35435cfb51
irasemcai <36616732+irasemcai@users.noreply.github.com>
2020-03-23 15:05:45 -0600

    Actividad 4
    Se almacena en un archivo txt los mensajes leidos por consola de un proceso

---
+++ ComunicacionEntreProcesos2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace ComunicacionEntreProcesos2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {                        
+            instrucciones();
+        }
+
+        public static void instrucciones()
+        {
+            Process proceso = Process.GetCurrentProcess();
+           
+            Console.WriteLine("1. Mandar Mensaje 2. Leer mensajes");
+            string opcion = Console.ReadLine();
+            
+            switch (opcion)
+            {
+                case "1":
+                    StreamWriter writer = File.CreateText("ProcesoFile.txt");
+                    Console.WriteLine("Escribe tu mensaje: ");
+                    string mensaje = Console.ReadLine();
+
+                    writer.WriteLine("ID: " + proceso.Id + " Hora: " + proceso.StartTime + " Mensaje: " + mensaje);
+
+                    writer.Close();
+                    break;
+
+                case "2":
+                    StreamReader reader = File.OpenText("ProcesoFile.txt");
+                    string linea= "";
+                    while(linea != null)
+                    {
+                        linea = reader.ReadLine();
+                        Console.WriteLine(linea);
+                    }
+                    reader.Close();
+                    break;
+             
+
+            }
+        }
+    }
+}
